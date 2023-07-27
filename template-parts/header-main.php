928bced6054134876dd187a51b3f732a0d09b15a
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-07-27 16:01:13 +0100

    up

--- template-parts/header-main.php
+++ template-parts/header-main.php
@@ -20,18 +20,36 @@
 			</div>
 
 			<div class="cta-buttons flex items-center gap-3 invisible md:visible">
-				<a class="btn btn--secondary" href="">
-					<?php esc_html_e( 'Contact sales', 'brister' ); ?>
-					<svg xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none">
-						<path d="M1.5 8.89999L5.17453 5.60706C5.60849 5.21818 5.60849 4.58181 5.17453 4.19292L1.5 0.899994" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-				</a>
-				<a class="btn btn--primary" href="">
-					<?php esc_html_e( 'Go to shop', 'brister' ); ?>
-					<svg xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none">
-						<path d="M1.5 8.89999L5.17453 5.60706C5.60849 5.21818 5.60849 4.58181 5.17453 4.19292L1.5 0.899994" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-					</svg>
-				</a>
+				<?php
+				$link_sales = get_field( 'cta_buttons_contact_sales', 'options' );
+				if ( $link_sales ) :
+					$link_url    = $link_sales['url'];
+					$link_title  = $link_sales['title'];
+					$link_target = $link_sales['target'] ? $link_sales['target'] : '_self';
+					?>
+					<a class="btn btn--secondary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+						<?php echo esc_html( $link_title ); ?>
+						<svg xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none">
+							<path d="M1.5 8.89999L5.17453 5.60706C5.60849 5.21818 5.60849 4.58181 5.17453 4.19292L1.5 0.899994" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</a>
+					<?php
+				endif;
+				$link_shop = get_field( 'cta_buttons_got_to_shop', 'options' );
+				if ( $link_shop ) :
+					$link_url    = $link_shop['url'];
+					$link_title  = $link_shop['title'];
+					$link_target = $link_shop['target'] ? $link_shop['target'] : '_self';
+					?>
+					<a class="btn btn--primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+						<?php echo esc_html( $link_title ); ?>
+						<svg xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none">
+							<path d="M1.5 8.89999L5.17453 5.60706C5.60849 5.21818 5.60849 4.58181 5.17453 4.19292L1.5 0.899994" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</a>
+					<?php
+				endif;
+				?>
 			</div>
 		</div>
 
--- template-parts/posts/navigation.php
+++ template-parts/posts/navigation.php
@@ -2,7 +2,7 @@
 	<?php
 	$args = array(
 		'prev_text' => sprintf( esc_html__( 'Previous post', 'brister' ), '<span class="meta-nav"> < </span>' ),
-		'next_text' => sprintf( esc_html__( 'Newer post %s', 'brister' ), '<span class="meta-nav"> > </span>' )
+		'next_text' => sprintf( esc_html__( 'Newer post', 'brister' ), '<span class="meta-nav"> > </span>' )
 	);
 	$navigation = get_the_post_navigation( $args );
 	if ( $navigation ) :
