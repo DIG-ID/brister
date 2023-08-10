0dff34319ceab969fd327f48a15d14df5c996917
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-08-10 21:41:24 +0100

    up

--- template-parts/header-simple.php
+++ template-parts/header-simple.php
@@ -1,6 +1,6 @@
-<header id="header-main" class="header-main w-full fixed top-0 z-10" itemscope itemtype="http://schema.org/WebSite">
-	<nav class="navigation-main px-4 md:px-8  md:py-6" role="navigation" aria-label="<?php esc_attr_e( 'main navigation', 'brister' ); ?>">
-		<div class="main-header-content flex columns-2 justify-between">
+<header id="header-main" class="header-main--simple w-full fixed top-0 z-10" itemscope itemtype="http://schema.org/WebSite">
+	<nav class="navigation-main px-4 md:px-8 py-4 md:py-6" role="navigation" aria-label="<?php esc_attr_e( 'main navigation', 'brister' ); ?>">
+		<div class="main-header-content flex lg:columns-2 justify-center lg:justify-between">
 
 
 			<div class="site-branding">
@@ -11,11 +11,11 @@
 					echo '<p class="screen-reader-text">' . get_bloginfo( 'name' ) . '</p>';
 				endif;
 				?>
-				<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url" class="navbar-brand custom-logo-link "><?php do_action( 'theme_logo' ); ?></a>
+				<span class="navbar-brand custom-logo-link "><?php do_action( 'theme_logo' ); ?></span>
 			</div>
 
 
-			<div class="cta-buttons flex items-center gap-3 invisible md:visible">
+			<div class="cta-buttons lg:flex items-center gap-3 hidden invisible md:visible">
 				<?php
 				$link_shop = get_field( 'cta_buttons_got_to_shop', 'options' );
 				if ( $link_shop ) :
--- template-parts/pages/qrcode/header.php
+++ template-parts/pages/qrcode/header.php
@@ -1,4 +1,4 @@
-<header class="page-header grid cols-1 lg:grid-cols-12 gap-4 px-8" style="background: linear-gradient(180deg, rgba(3, 47, 53, 0.50) 0%, rgba(16, 102, 79, 0.50) 100%), url('<?php echo esc_url( get_the_post_thumbnail_url( get_the_ID(), 'full' ) ); ?>'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+<header class="page-header grid cols-1 lg:grid-cols-12 gap-4 px-4 lg:px-8" style="background: linear-gradient(180deg, rgba(3, 47, 53, 0.50) 0%, rgba(16, 102, 79, 0.50) 100%), url('<?php echo esc_url( get_the_post_thumbnail_url( get_the_ID(), 'full' ) ); ?>'); background-position: center; background-repeat: no-repeat; background-size: cover;">
 	<div class="  lg:col-start-2 lg:col-span-5 py-32 lg:py-48">
 		<h1 class="font-montbold font-extrabold text-white text-3xl lg:text-6xl uppercase mb-8"><?php the_title(); ?></h1>
 		<p class="description-section text-light-green !mb-0"><?php echo wp_kses_post( wp_strip_all_tags( get_the_content() ) ); ?></p>
