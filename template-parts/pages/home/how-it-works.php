c925726ec09fa92839167e9b1f799725c2627198
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-08-11 10:32:52 +0100

    up

--- template-parts/pages/home/how-it-works.php
+++ template-parts/pages/home/how-it-works.php
@@ -1,5 +1,5 @@
 <section class="section-how-it-works py-20 md:py-36 px-4 md:px-8 w-full bg-dark-green grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
-	<div class="md:col-span-5 lg:col-span-4 md:col-start-1 lg:col-start-2 md:text-end mb-12 md:mb-0">
+	<div class="md:col-span-5 xl:col-span-4 md:col-start-1 xl:col-start-2 md:text-end mb-12 md:mb-0">
 		<h2 class="font-montbold text-4xl lg:text-6xl text-light-green uppercase mb-6"><?php echo wp_kses_post( get_field( 'how_it_works_title' ) ); ?></h2>
 		<p class="font-montlight text-lg text-light-green leading-8 mb-4"><?php echo wp_kses_post( get_field( 'how_it_works_description' ) ); ?></p>
 		<?php
@@ -14,7 +14,7 @@
 		endif;
 		?>
 	</div>
-	<div class="md:col-span-6 md:col-start-7 lg:col-span-4 lg:col-start-7">
+	<div class="md:col-span-6 md:col-start-7 xl:col-span-4 xl:col-start-7">
 		<?php
 		if ( have_rows( 'how_it_works_list' ) ) :
 			echo '<ol class="list-decimal marker:text-lime-green marker:text-4xl lg:marker:text-6xl marker:font-montbold">';
