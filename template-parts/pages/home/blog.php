951b2b13706b8e6104b2e8b917269b355630746c
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-08-16 13:01:14 +0100

    up

--- template-parts/pages/home/blog.php
+++ template-parts/pages/home/blog.php
@@ -1,7 +1,7 @@
 <section class="section-blog pt-20 md:pt-36 pb-24 md:pb-40 px-4 md:px-8 w-full bg-dark-green text-center">
 	<div class="text-center md:px-8 max-w-full md:max-w-screen-md mx-auto">
-		<h2 class="title-section text-light-green"><?php esc_html_e( 'Blog', 'brister' ); ?></h2>
-		<p class="description-section text-light-green">Gutes und gesundes Essen ist Lebensqualität.<br> Zusammen mit unseren Regionalen Partner bieten.</p>
+		<h2 class="title-section text-light-green"><?php the_field( 'blog_section_title' ); ?></h2>
+		<p class="description-section text-light-green"><?php the_field( 'blog_section_description' ); ?></p>
 	</div>
 	<div class="blog-grid w-full grid grid-cols-1 lg:grid-cols-12 gap-8">
 		<?php
