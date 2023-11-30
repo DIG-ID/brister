12e25df6d3fe6d5c9fe2ab781d18e17cf949cad3
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-11-30 16:00:08 +0000

    up

--- template-parts/pages/about/intro.php
+++ template-parts/pages/about/intro.php
@@ -1,4 +1,4 @@
-<section class="section-intro w-full py-20 lg:py-32 px-4 lg:px-8 text-center grid grid-cols-1 lg:grid-cols-12 lg:gap-4">
+<section class="section-intro w-full py-20 lg:py-32 px-4 lg:px-8 text-center grid grid-cols-1 lg:grid-cols-12 lg:gap-4 mb-20">
 	<div class="lg:col-span-8 lg:col-start-3">
 		<h2 class="title-section text-green"><?php the_field( 'intro_title' ); ?></h2>
 		<p class="text-body text-dark-green"><?php the_field( 'intro_description' ); ?></p>
